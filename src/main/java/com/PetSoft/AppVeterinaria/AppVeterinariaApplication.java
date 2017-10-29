7e1fde1d6fc6f06d274d68af64d2f6b26b60e987
Jose Luis Escobar Ramirez <joseluise9090gmail.com>
2017-10-29 14:59:59 -0500

    Create

---
+++ src/main/java/com/PetSoft/AppVeterinaria/AppVeterinariaApplication.java
@@ -0,0 +1,12 @@
+package com.PetSoft.AppVeterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVeterinariaApplication.class, args);
+	}
+}
